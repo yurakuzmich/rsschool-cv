9ee93b35c5850916e2c0c626fc9d9b6c808ec054
Yury Kuzmich <yurakuzmich@gmail.com>
2020-10-05 10:48:29 +0300

    feat: Add smooth scrolling to element after menu-item clicked

--- code.js
+++ code.js
@@ -1,9 +1,30 @@
 window.onload = function() {
     const slider = document.querySelector('.slider');
-    slider.addEventListener('click', toggleSliderClass)
+    slider.addEventListener('click', toggleSliderClass);
+
+    //add Listeners to menu
+    const mainMenu = document.querySelector(".main-navigation");
+    mainMenu.addEventListener('click', (event) => {
+        event.preventDefault();
+        let clickedElement = event.target;
+        slideToClickedElement(clickedElement);
+    });
+
 }
 
 function toggleSliderClass() {
     let slider = document.querySelector('.slider');
     slider.classList.toggle('slider_alternate');
+}
+
+function slideToClickedElement(sliderTarget) {
+    let targetHref = sliderTarget.getAttribute('href').substr(1);
+    target = document.getElementById(targetHref);
+    if(target === null) {
+        target = document.getElementById('header');
+    }
+    target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
 }
