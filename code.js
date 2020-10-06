24418158501c34ea51094a77b73149ac47d6d8ec
Yury Kuzmich <yurakuzmich@gmail.com>
2020-10-06 15:52:20 +0300

    feat: create shuffled portfolio

--- code.js
+++ code.js
@@ -28,6 +28,18 @@ window.onload = function() {
         // console.log(event.target.tagName);
     });
 
+    //add listener to portfolio tags
+    const portfolioTags = document.querySelector('.portfolio-head-tags');
+    portfolioTags.addEventListener('click', function(event) {
+        event.preventDefault();
+        let clickedElement = event.target;
+        if(clickedElement.tagName === 'A') {
+            // shufflePortfolio();
+            shufflePortfolioByOrder();
+        }
+    });
+    
+
     //addListener toTopButton 
     // const toTopButton = document.getElementById('to-top-button');
     // toTopButton.addEventListener('click', goToTopOfPage);
@@ -59,3 +71,31 @@ function slideToClickedElement(sliderTarget) {
 //     });
 //     toTopButton.classList.toggle('to-top-button_hidden');
 // }
+
+//SHUFFLE PORTFOLIO IMAGES
+function shufflePortfolio() {
+    let portfolioImages = document.querySelectorAll('.portfolio-images-list__item');
+    // console.log(portfolioImages);
+    for(let i = portfolioImages.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let t = portfolioImages[i];
+        portfolioImages[i] = portfolioImages[j];
+        portfolioImages[j] = t;
+        // [portfolioImages[i], portfolioImages[j]] = [portfolioImages[j], portfolioImages[i]];
+    }
+    
+    let portfolioImagesList = document.querySelector('.portfolio-images-list');
+    let newInnerHtml = '';
+    for(let i = 0; i < portfolioImages.length; i++) {
+        newInnerHtml += portfolioImages[i];
+        console.log(portfolioImages[i]);
+    }
+    portfolioImagesList.innerHTML = newInnerHtml;
+}
+
+function shufflePortfolioByOrder() {
+    let portfolioImages = document.querySelectorAll('.portfolio-images-list__item');
+    for(let i = 0; i < portfolioImages.length; i++) {
+        portfolioImages[i].style.order = Math.floor(100 * Math.random());
+    }
+}
