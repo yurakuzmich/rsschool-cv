052e88d953ae14dc6eaeed2b06933d090daad945
Yury Kuzmich <yurakuzmich@gmail.com>
2020-10-09 13:19:42 +0300

    feat: add link activating with scroll

--- code.js
+++ code.js
@@ -1,18 +1,11 @@
 $(document).ready(function(){
-    $('.slider-window').bxSlider({
-        pager: 'false',
-        controls: 'true',
-        nextText: 'Uprava'
-    });
+    $('.slider-window').bxSlider();
   });
 
 window.onload = function() {
-    // const slider = document.querySelector('.slider');
-    // slider.addEventListener('click', toggleSliderClass);
 
-    // window.addEventListener('click', function() {
-    //     console.log('Portfolio offset is ' + window.scrollTop);
-    // });
+    //Add listeners for scrolling
+    document.addEventListener('scroll', onScroll);
 
     //add Listeners to menu
     const mainMenu = document.querySelector(".main-navigation");
@@ -27,7 +20,7 @@ window.onload = function() {
 
     //add Listeners to burger menu
     const burgerMenu = document.querySelector('.burger-menu_box');
-    burgerMenu.addEventListener('click', function(event) {
+    burgerMenu.addEventListener('click', (event) => {
         event.preventDefault();
         let clickedElement = event.target;
         if(clickedElement.tagName === 'A') {
@@ -35,7 +28,6 @@ window.onload = function() {
             let burgerMenuCheckbox = document.getElementById('burger-menu__toggle');
             burgerMenuCheckbox.checked = false;
         }
-        // console.log(event.target.tagName);
     });
 
     //add listener to portfolio tags
@@ -45,20 +37,9 @@ window.onload = function() {
         let clickedElement = event.target;
         if(clickedElement.tagName === 'A') {
             shufflePortfolio();
-            // shufflePortfolioByOrder();
             setActiveTag(clickedElement);
         }
     });
-    
-
-    //addListener toTopButton 
-    // const toTopButton = document.getElementById('to-top-button');
-    // toTopButton.addEventListener('click', goToTopOfPage);
-}
-
-function toggleSliderClass() {
-    let slider = document.querySelector('.slider');
-    slider.classList.toggle('slider_alternate');
 }
 
 function slideToClickedElement(sliderTarget) {
@@ -73,31 +54,17 @@ function slideToClickedElement(sliderTarget) {
     });
 }
 
-// function goToTopOfPage(event) {
-//     let target = document.getElementById('header');
-//     let toTopButton = event.target;
-//     target.scrollIntoView({
-//         behavior: 'smooth',
-//         block: 'start'
-//     });
-//     toTopButton.classList.toggle('to-top-button_hidden');
-// }
 
 //SHUFFLE PORTFOLIO IMAGES
 function shufflePortfolio() {
     let portfolioImages = document.querySelectorAll('.portfolio-images-list__item');
-    // console.log(portfolioImages);
     let portfolioImagesArray = Array.prototype.slice.call(portfolioImages);
     for(let i = portfolioImagesArray.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
-        
         let t = portfolioImagesArray[i];
         portfolioImagesArray[i] = portfolioImagesArray[j];
         portfolioImagesArray[j] = t;
-        // [portfolioImages[i], portfolioImages[j]] = [portfolioImages[j], portfolioImages[i]];
-        // console.log(portfolioImagesArray[i]);
     }
-    
     let portfolioImagesList = document.querySelector('.portfolio-images-list');
     let newInnerHtml = '';
     for(let i = 0; i < portfolioImagesArray.length; i++) {
@@ -106,28 +73,33 @@ function shufflePortfolio() {
     portfolioImagesList.innerHTML = newInnerHtml;
 }
 
-function shufflePortfolioByOrder() {
-    let portfolioImages = document.querySelectorAll('.portfolio-images-list__item');
-    for(let i = 0; i < portfolioImages.length; i++) {
-        portfolioImages[i].style.order = Math.floor(100 * Math.random());
-        portfolioImages[i].style.margin = '0 20px 20px 0';
-    }
-}
-
 function setActiveTag(targetElement){
    let currentSelectedElement = document.querySelector('.portfolio-head-tags__item_selected');
-//    console.log(currentSelectedElement);
    currentSelectedElement.classList.remove('portfolio-head-tags__item_selected');
     targetElement.classList.add('portfolio-head-tags__item_selected');
 }
 
 function setActiveMenuItem(targetElement) {
     let currentSelectedElement = document.querySelector('.main-navigation-menu__item_active');
- //    console.log(currentSelectedElement);
     currentSelectedElement.classList.remove('main-navigation-menu__item_active');
      targetElement.classList.add('main-navigation-menu__item_active');
  }
 
- function getAnchors() {
+ function onScroll() {
+    const currentPosition = window.scrollY + 96;
+    const divs = document.querySelectorAll('#main > div > div');
+    const links = document.querySelectorAll('.main-navigation-menu__item');
+    
+    divs.forEach((el) => {
 
+        if(el.offsetTop <= currentPosition && (el.offsetTop + el.offsetHeight) > currentPosition) {
+            links.forEach((a) => {
+                a.classList.remove('main-navigation-menu__item_active');
+                if(el.getAttribute('id') === a.getAttribute('href').substr(1)) {
+                    a.classList.add('main-navigation-menu__item_active');
+                }
+            });
+        }
+
+    });
  }
