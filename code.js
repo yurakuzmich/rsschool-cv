948b5360c239fa8d428cda39d44d26769e5ec32c
Yury Kuzmich <yurakuzmich@gmail.com>
2020-10-16 13:54:04 +0300

    fix: use some logic from original example

--- code.js
+++ code.js
@@ -5,8 +5,8 @@ const CButton = document.getElementById('c');
 const actionButtons = document.querySelectorAll('.math_action');
 const numbersButtons = document.querySelectorAll('.number');
 const commaButton = document.getElementById('comma');
-const switchNegativeButton = document.getElementById('switch-negative');
-const resultButton = document.getElementById('result_button');
+// const switchNegativeButton = document.getElementById('switch-negative');
+const percentButton = document.getElementById('percent');
 const howToButton = document.getElementById('howitwork-button');
 const aboutButton = document.getElementById('about-button');
 const myModal = document.getElementById('modal__wrapper');
@@ -17,20 +17,22 @@ const myModalHeader = document.getElementById('modal__header');
 const myModalText = document.getElementById('modal__text');
 
 const howToHeader = 'Руководство пользователя';
-const howToManual = '<p>C - полный сброс</p>' + 
-                    '<p>CE - очистка дисплея. Число в памяти сохраняется</p>';
+const howToManual = '<p>C - полный сброс</p>' +
+    '<p>CE - очистка дисплея. Число в памяти сохраняется</p>';
 
 const aboutHeader = 'О программе';
 const aboutText = '<p>Мощный, быстрый, продвинутый калькулятор.</p>' +
-                    '<p>Надежные, сверхсовременные алгоритмы позволяют складывать, отнимать, умножать делить небольшие числа с небывалой точность!</p>';
+    '<p>Надежные, сверхсовременные алгоритмы позволяют складывать, отнимать, умножать делить небольшие числа с небывалой точность!</p>';
+
+const percentHeader = 'К сожалению, кнопка не работает';
+const percentText = '<p>Я долго пытался понять логику работы этой кнопки на простом калькуляторе, но понял только для сложения и вычитания.</p>' +
+    '<p>Поэтому я просто взял и отключил ее к чертям. Ну и вывел это чудесное модальное окошко.</p>';
 
 let operationMemory = 0;
-let isOperationStarted = false;
 let currentOperation = '';
-let currentValue = 0;
 let newOperand = false;
-let resultPressed = false;
-let commaClicked = false;
+
+
 
 
 window.onload = function () {
@@ -38,7 +40,7 @@ window.onload = function () {
     CEButton.addEventListener('click', clickCE);
     CButton.addEventListener('click', clickC);
     commaButton.addEventListener('click', clickComma);
-    switchNegativeButton.addEventListener('click', clickNegativeButton);
+    // switchNegativeButton.addEventListener('click', clickNegativeButton);
     for (let i = 0; i <= actionButtons.length - 1; i++) {
         let actionButton = actionButtons[i];
         actionButton.addEventListener('click', clickActionButton);
@@ -47,7 +49,7 @@ window.onload = function () {
         let numberButton = numbersButtons[i];
         numberButton.addEventListener('click', clickNumberButton);
     }
-    resultButton.addEventListener('click', clickResultButton);
+    percentButton.addEventListener('click', clickPercentButton);
     howToButton.addEventListener('click', clickHowToButton);
     aboutButton.addEventListener('click', clickAboutButton);
     myModalClose.addEventListener('click', switсhModal);
@@ -59,143 +61,104 @@ window.onload = function () {
 //FUNCTIONS
 function clickCE() {
     display.value = 0;
-    commaClicked = false;
-    logAllVars();
+    newOperand = true;
 }
 
 function clickC() {
     display.value = 0;
     operationMemory = 0;
-    isOperationStarted = false;
     currentOperation = '';
-    currentValue = 0;
-    commaClicked = false;
-    logAllVars();
+    newOperand = true;
 }
 
 function clickComma() {
-    if (commaClicked === false) {
-        display.value = display.value + '.';
-        commaClicked = true;
-    } else if (resultPressed === true) {
+
+    if (newOperand === true) {
         display.value = '0.';
-        commaClicked = true;
+        newOperand = false;
+    } else {
+        if (display.value.indexOf('.') === -1) {
+            display.value += '.';
+        }
     }
-    logAllVars();
 }
 
 function clickActionButton(event) {
 
-    isOperationStarted = true;
-    currentOperation = event.target.textContent;
-    newOperand = true;
-    commaClicked = false;
-    operationMemory = Number(display.value);
+    let localOperationMemory = display.value;
+
+    if (newOperand === true && currentOperation !== '=') {
+        display.value = operationMemory;
+    } else {
+        newOperand = true;
+        if (currentOperation === '+') {
+            operationMemory += +localOperationMemory;
+        } else if (currentOperation === '-') {
+            operationMemory -= +localOperationMemory;
+        } else if (currentOperation === 'x') {
+            operationMemory *= +localOperationMemory;
+        } else if (currentOperation === '/') {
+            operationMemory /= +localOperationMemory;
+        } else {
+            operationMemory = +localOperationMemory;
+        }
+        display.value = operationMemory;
+        currentOperation = event.target.textContent;
+
+    }
     logAllVars();
 }
+
+
 
 function clickNumberButton(event) {
-
-    switch (isOperationStarted) {
-        case true:
-            if (newOperand === true) {
-                display.value = '';
-                newOperand = false;
-            }
-            if (display.value.length <= displayDigits) {
-                display.value = Number(display.value += event.target.textContent);
-            }
-            break;
-        case false:
-            if (display.value === 0) {
-                if (resultPressed == true) {
-                    display.value += event.target.textContent;
-                    resultPressed = false;
-                    commaClicked = false;
-                } else {
-                    display.value += event.target.textContent;
-                    commaClicked = false;
-                }
-            } else if (display.value.length <= displayDigits) {
-                display.value = Number(display.value += event.target.textContent);
-            }
-            break;
+    let pressedNumber = event.target.textContent;
+    if (newOperand === true) {
+        display.value = pressedNumber;
+        newOperand = false;
+    } else {
+        if (display.value === '0') {
+            display.value = pressedNumber;
+        } else {
+            display.value += pressedNumber;
+        }
     }
     logAllVars();
 }
 
-function clickNegativeButton() {
-    display.value = (-1) * display.value;
-    logAllVars();
+// function clickNegativeButton() {
+//     display.value = (-1) * display.value;
+//     logAllVars();
+// }
+
+function clickPercentButton() {
+    switсhModal();
+    myModalHeader.innerHTML = percentHeader;
+    myModalText.innerHTML = percentText;
 }
 
-function clickResultButton() {
-    let result = 0;
-    commaClicked = true;
-    switch (currentOperation) {
-        case '+':
-            result = Number(operationMemory) + Number(display.value);
-            break;
-        case '-':
-            result = Number(operationMemory) - Number(display.value);
-            break;
-        case 'x':
-            result = Number(operationMemory) * Number(display.value);
-            break;
-        case '/':
-            result = Number(operationMemory) / Number(display.value);
-            result = String(result).slice(0, displayDigits);
-            break;
-        case '':
-            result = 0;
-            commaClicked = false;
-            break;
-        default:
-            result = 0;
-            commaClicked = false;
-    }
-
-    if (result.length > displayDigits) {
-        result = result.slice(0, displayDigits);
-    }
-
-    display.value = result;
-    operationMemory = result;
-    isOperationStarted = false;
-    currentOperation = '';
-    currentValue = 0;
-    newOperand = false;
-    resultPressed = true;
-    logAllVars();
-
-}
 
 function switсhModal() {
     myModal.classList.toggle('modal_hidden');
 }
 
 function clickHowToButton() {
-    console.log('Howto button clicked');
     switсhModal();
     myModalHeader.innerHTML = howToHeader;
     myModalText.innerHTML = howToManual;
 }
 
 function clickAboutButton() {
-    console.log('About button clicked');
     switсhModal();
     myModalHeader.innerHTML = aboutHeader;
     myModalText.innerHTML = aboutText;
 }
 
 function logAllVars() {
-    console.log(`Display value is ${display.value}`);
-    console.log(`Operation memory is ${operationMemory}`);
-    console.log(`Operation started is ${isOperationStarted}`);
-    console.log(`Current operation is ${currentOperation}`);
-    console.log(`Current Value is ${currentValue}`);
-    console.log(`New operand is ${newOperand}`);
-    console.log(`Comma clicked is ${commaClicked}`);
-    console.log(`Result pressed is ${resultPressed}`);
-    console.log('-------------');
-}  
+    console.log(`Display shows ${display.value}`);
+    console.log(`operationMemory is ${operationMemory}`);
+    console.log(`currentOperation is ${currentOperation}`);
+    console.log(`newOperand is ${newOperand}`);
+    console.log(`------------------------`);
+
+}
