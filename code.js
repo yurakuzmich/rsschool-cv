335a1fecf879a93f6b38616670fd08fc969a79c3
Yury Kuzmich <yurakuzmich@gmail.com>
2020-10-18 14:27:05 +0300

    feat: add backgrounds

--- code.js
+++ code.js
@@ -11,6 +11,7 @@ console.log(`time is ${time} \n greeting is ${greeting} \n name is ${name} \n fo
 amPm.addEventListener('click', switchTimeFormat);
 showTime();
 setInterval(showTime, 1000);
+setBackgroundImage();
 
 
 function showTime() {
@@ -19,19 +20,28 @@ function showTime() {
     let hours = currTime.getHours();
     let minutes = currTime.getMinutes();
     let seconds = currTime.getSeconds();
+    let partOfTheDay = '';
+    if (hours < 12) {
+        partOfTheDay = 'AM'
+    } else {
+        partOfTheDay = 'PM';
+    }
     if (minutes < 10) {
         minutes = '0' + minutes;
     }
     if (seconds < 10) {
         seconds = '0' + seconds;
     }
-    if(timeFormat === 12) {
+    if (timeFormat === 12) {
         hours = hours - 12;
-        if(hours === 0) {
+        if (hours === 0) {
             hours = 12;
         }
     }
     userTime = `${hours}:${minutes}:${seconds}`;
+    if (timeFormat === 12) {
+        userTime += ` ${partOfTheDay}`;
+    }
     time.innerHTML = userTime;
 }
 
@@ -43,6 +53,58 @@ function switchTimeFormat() {
             break;
         case 24:
             timeFormat = 12;
-            amPm.innerHTML =  '<p>12/<span>24</span></p>'
+            amPm.innerHTML = '<p>12/<span>24</span></p>'
+    }
+}
+
+function setBackgroundImage() {
+    let currTime = new Date();
+    let hours = currTime.getHours();
+    if (hours >=22 || hours < 6) {
+        document.body.style.backgroundImage = 'url("images/night.jpg")';
+        greeting.textContent = 'Good night, ';
+        document.body.style.color = '#ffffff';
+        console.log('its night');
+    } else if (hours >= 6 && hours < 9) {
+        document.body.style.backgroundImage = 'url("images/morning.jpg")';
+        greeting.textContent = 'Good morning, ';
+        document.body.style.color = '#000000';
+        console.log('its morning');
+    } else if (hours >= 9 && hours < 18) {
+        document.body.style.backgroundImage = 'url("images/day.jpg")';
+        greeting.textContent = 'Good day, ';
+        document.body.style.color = '#000000';
+        console.log('its day');
+    } else if (hours >= 18 && hours < 22) {
+        document.body.style.backgroundImage = 'url("images/evening.jpg")';
+        greeting.textContent = 'Good evening, ';
+        document.body.style.color = '#ffffff';
+        
+        console.log('its day');
+    }
+
+
+    switch (hours) {
+        case hours < 24:
+            document.body.style.backgroundImage = 'url("images/night.jpg")';
+            
+            console.log('its night');
+            break;
+        case hours < 21:
+            document.body.style.backgroundImage = 'url("images/evening.jpg")';
+            console.log('its evening');
+            break;
+        case hours < 18:
+            document.body.style.backgroundImage = 'url("images/day.jpg")';
+            console.log('its day');
+            break;
+        case hours < 10:
+            document.body.style.backgroundImage = 'url("images/morning.jpg")';
+            console.log('its morning');
+            break;
+        case hours < 6:
+            document.body.style.backgroundImage = 'url("images/night.jpg")';
+            console.log('its night');
+            break;
     }
 }
