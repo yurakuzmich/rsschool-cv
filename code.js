4e7a07de927f13373601cde2edd2dad3a6018336
Yury Kuzmich <yurakuzmich@gmail.com>
2020-10-18 16:22:22 +0300

    feat: add localStorage functions

--- code.js
+++ code.js
@@ -1,16 +1,65 @@
 const time = document.getElementById('time'),
     greeting = document.getElementById('greeting'),
     name = document.getElementById('name'),
-    focus = document.getElementById('focus'),
+    myFocus = document.getElementById('focus'),
     amPm = document.getElementById('am-pm');
+    delSettings = document.getElementById('clearstorage');
 
 let timeFormat = 24;
 
 //listeners
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+myFocus.addEventListener('keypress', setTask);
+myFocus.addEventListener('blur', setTask);
 amPm.addEventListener('click', switchTimeFormat);
+delSettings.addEventListener('click', clearSettings);
 showTime();
 setInterval(showTime, 1000);
 setBackgroundImage();
+getName();
+getTask();
+
+
+function getTask() {
+    if (localStorage.getItem('task') === null) {
+        myFocus.textContent = '[Enter Task]';
+    } else {
+        myFocus.textContent = localStorage.getItem('task');
+    }
+}
+
+function setTask(event){
+    if (event.type === 'keypress') {
+        // Make sure enter is pressed
+        if (event.keyCode == 13) {
+            localStorage.setItem('task', event.target.innerText);
+            myFocus.blur();
+        }
+    } else {
+        localStorage.setItem('task', event.target.innerText);
+    }
+}
+
+function getName() {
+    if (localStorage.getItem('name') === null) {
+        name.textContent = '[Enter Name]';
+    } else {
+        name.textContent = localStorage.getItem('name');
+    }
+}
+
+function setName(event) {
+    if (event.type === 'keypress') {
+        // Make sure enter is pressed
+        if (event.keyCode == 13) {
+            localStorage.setItem('name', event.target.innerText);
+            name.blur();
+        }
+    } else {
+        localStorage.setItem('name', event.target.innerText);
+    }
+}
 
 function showTime() {
     let currTime = new Date();
@@ -58,7 +107,7 @@ function switchTimeFormat() {
 function setBackgroundImage() {
     let currTime = new Date();
     let hours = currTime.getHours();
-    if (hours >=22 || hours < 6) {
+    if (hours >= 22 || hours < 6) {
         document.body.style.backgroundImage = 'url("images/night.jpg")';
         greeting.textContent = 'Good night, ';
         document.body.style.color = '#ffffff';
@@ -68,19 +117,32 @@ function setBackgroundImage() {
         greeting.textContent = 'Good morning, ';
         document.body.style.color = '#000000';
         console.log('its morning');
-    } else if (hours >= 9 && hours < 18) {
+    } else if (hours >= 9 && hours < 16) {
         document.body.style.backgroundImage = 'url("images/day.jpg")';
         greeting.textContent = 'Good day, ';
         document.body.style.color = '#000000';
         console.log('its day');
-    } else if (hours >= 18 && hours < 22) {
+    } else if (hours >= 16 && hours < 22) {
         document.body.style.backgroundImage = 'url("images/evening.jpg")';
         greeting.textContent = 'Good evening, ';
         document.body.style.color = '#000000';
-        
+
         console.log('its day');
     }
 }
+
+function clearSettings() {
+    localStorage.clear();
+    getName();
+    getTask();
+}
+
+
+navigator.geolocation.getCurrentPosition(function(position) {
+    let myLat = position.coords.latitude;
+    let myLng = position.coords.longitude;
+    console.log(myLat + ' + ' + myLng);
+});
 
 
 
