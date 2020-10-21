c95761c5d5f2f114c604e775893383bb349c3ef3
Yury Kuzmich <yurakuzmich@gmail.com>
2020-10-21 19:06:27 +0300

    fead: add click events

--- code.js
+++ code.js
@@ -6,9 +6,26 @@ console.log(keys);
 
 window.addEventListener('keydown', playSound);
 for(let i = 0; i < keys.length; i++) {
+    console.log(keys[i]);
+    keys[i].addEventListener('click', playSoundOnKlick);
     keys[i].addEventListener('transitionend', removeKeyStyle);
+    
 }
 
+function playSoundOnKlick(event) {
+    let dataKey = event.target.dataset.key;
+    console.log(dataKey);
+    let audio = document.querySelector(`audio[data-key="${dataKey}"]`);
+    let key = document.querySelector(`.key[data-key="${dataKey}"]`);
+    //Audio
+    if (!audio) {
+        return;
+    }
+    audio.currentTime = 0;
+    audio.play();
+    //Key changing
+    key.classList.add('pressed');
+}
 
 function playSound(event) {
     let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
