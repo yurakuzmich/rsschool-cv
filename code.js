29b4f144d490ebed48e8c44626f88056699c19e3
Yury Kuzmich <yurakuzmich@gmail.com>
2020-10-23 09:39:59 +0300

    feat: add range time-picker and some custom styles

--- code.js
+++ code.js
@@ -10,73 +10,75 @@ const timeDisplay = document.querySelector(".time-display");
 const outlineLength = outline.getTotalLength();
 //Duration
 const timeSelect = document.querySelectorAll(".time-select button");
+const timeSelectRange = document.querySelector(".rangemins");
 let fakeDuration = 600;
 
 outline.style.strokeDashoffset = outlineLength;
 outline.style.strokeDasharray = outlineLength;
 timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
-  fakeDuration % 60
+    fakeDuration % 60
 )}`;
 
 sounds.forEach(sound => {
-  sound.addEventListener("click", function() {
-    song.src = this.getAttribute("data-sound");
-    video.src = this.getAttribute("data-video");
+    sound.addEventListener("click", function () {
+        song.src = this.getAttribute("data-sound");
+        video.src = this.getAttribute("data-video");
+        checkPlaying(song);
+    });
+});
+
+play.addEventListener("click", function () {
     checkPlaying(song);
-  });
 });
 
-play.addEventListener("click", function() {
-  checkPlaying(song);
-});
-
-replay.addEventListener("click", function() {
+replay.addEventListener("click", function () {
     restartSong(song);
-    
-  });
+
+});
 
 
-const restartSong = song =>{
+const restartSong = song => {
     let currentTime = song.currentTime;
     song.currentTime = 0;
-    console.log("ciao")
-
 }
 
 timeSelect.forEach(option => {
-  option.addEventListener("click", function() {
-    fakeDuration = this.getAttribute("data-time");
-    timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
-      fakeDuration % 60
-    )}`;
-  });
+    option.addEventListener("click", function () {
+        fakeDuration = this.getAttribute("data-time");
+        timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`;
+    });
+});
+timeSelectRange.addEventListener('change', function() {
+    fakeDuration = this.value;
+    console.log(fakeDuration);
+        timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}`;
 });
 
 const checkPlaying = song => {
-  if (song.paused) {
-    song.play();
-    video.play();
-    play.src = "./svg/pause.svg";
-  } else {
-    song.pause();
-    video.pause();
-    play.src = "./svg/play.svg";
-  }
+    if (song.paused) {
+        song.play();
+        video.play();
+        play.src = "./svg/pause.svg";
+    } else {
+        song.pause();
+        video.pause();
+        play.src = "./svg/play.svg";
+    }
 };
 
-song.ontimeupdate = function() {
-  let currentTime = song.currentTime;
-  let elapsed = fakeDuration - currentTime;
-  let seconds = Math.floor(elapsed % 60);
-  let minutes = Math.floor(elapsed / 60);
-  timeDisplay.textContent = `${minutes}:${seconds}`;
-  let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
-  outline.style.strokeDashoffset = progress;
+song.ontimeupdate = function () {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
+    timeDisplay.textContent = `${minutes}:${seconds}`;
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
 
-  if (currentTime >= fakeDuration) {
-    song.pause();
-    song.currentTime = 0;
-    play.src = "./svg/play.svg";
-    video.pause();
-  }
+    if (currentTime >= fakeDuration) {
+        song.pause();
+        song.currentTime = 0;
+        play.src = "./svg/play.svg";
+        video.pause();
+    }
 };
