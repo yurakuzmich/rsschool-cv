274f995f501c7677e5892ecada99a24a0817f062
Yury Kuzmich <yurakuzmich@gmail.com>
2020-11-02 22:14:16 +0300

    feat: add fonts and settings button

--- code.js
+++ code.js
@@ -1,16 +1,18 @@
 
 const newGame = document.querySelector('.newgame');
-const scoreBoard = document.querySelector('.scores p');
+const highScoreBoard = document.querySelector('.highscore p');
+const scoreBoard = document.querySelector('.score p');
 const game = document.querySelector('.game');
 const windows = document.querySelectorAll('.window');
 const enemyes = document.querySelectorAll('.enemy');
 const blood = document.querySelector('.blood');
-console.log(enemyes);
+
 let lastWindow;
 let gameDuration = 10000;
 let enemyKilled = false;
 let score = 0;
 let timeIsOwer = true;
+let myTimeout;
 //Listeners
 newGame.addEventListener('click', startGame);
 game.addEventListener('click', (e) => {
@@ -19,15 +21,14 @@ game.addEventListener('click', (e) => {
 });
 
 //Functions
-function getRandomTime(min = 300, max=1000) {
+function getRandomTime(min = 300, max = 1000) {
     return Math.round(Math.random() * (max - min) + min);
 }
 
 function getRandomWindow() {
     const rnd = Math.floor(Math.random() * windows.length);
     const randomWindow = windows[rnd];
-    if(randomWindow === lastWindow) {
-        console.log('Window was repeated');
+    if (randomWindow === lastWindow) {
         return getRandomWindow();
     }
     lastWindow = randomWindow;
@@ -41,42 +42,66 @@ function showEnemy() {
     window.classList.add('up');
     setTimeout(() => {
         window.classList.remove('up');
-        if(timeIsOwer === false) {
+        if (timeIsOwer === false ) {
             showEnemy();
         }
     }, time);
 }
 
+function hideAllEnemyes() {
+    windows.forEach((window) => {
+        window.classList.remove('up');
+    });
+}
+
 function checkShoot(e) {
-    if(!e.isTrusted) return; // cheater!
-    if(e.target.classList.contains('enemy') && enemyKilled === false) {
+    if (!e.isTrusted) return;
+    if (e.target.classList.contains('enemy') && enemyKilled === false) {
         score++;
         scoreBoard.textContent = `Current score: ${score}`;
         showBlood(e);
         enemyKilled = true;
     }
-    console.log(score);
 }
 
 function showBlood(e) {
-    // console.log(e);
-        console.log('Nice shoot');
-        blood.style.top = e.clientY - 35 + 'px';
-        blood.style.left = e.clientX - 35 + 'px';
-        setTimeout(() => {
-            blood.style.top = '-100px';
-            blood.style.left = '-100px';
-        }, 400);
+    console.log('Nice shoot');
+    blood.style.top = e.clientY - 35 + 'px';
+    blood.style.left = e.clientX - 35 + 'px';
+    setTimeout(() => {
+        blood.style.top = '-100px';
+        blood.style.left = '-100px';
+    }, 400);
+}
+
+function setHighScore() {
+    if(!localStorage.highscore) {
+        localStorage.setItem('highscore', score);
+        console.log(highScore);
+    } else if(localStorage.highscore < score) {
+        localStorage.highscore = score;
+    } 
+}
+
+function showHighScore() {
+    let msg = `Highscore: ${localStorage.highscore}`;
+    highScoreBoard.textContent = msg;
 }
 
 function startGame() {
+    clearTimeout(myTimeout);
+    hideAllEnemyes();
     score = 0;
     scoreBoard.textContent = `Current score: ${score}`;
     timeIsOwer = false;
     showEnemy();
-    setTimeout(() => {
+    myTimeout = setTimeout(() => {
         enemyKilled = true;
         timeIsOwer = true;
         scoreBoard.textContent = `Time is ower. You score is ${score}`;
+        setHighScore();
     }, gameDuration)
 }
+
+setHighScore();
+showHighScore();
