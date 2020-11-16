42827a781fa0bf89392b8b9d934c7143ded2e42b
Yury Kuzmich <yurakuzmich@gmail.com>
2020-11-16 21:49:01 +0300

    feat: add simple raindrop

--- code.js
+++ code.js
@@ -28,6 +28,9 @@ class Raindrop {
             left: 0,
         };
         this.raindropBody = document.createElement("div");
+        this.parentNode = document.querySelector('.game');
+        console.log(this.parentNode.offsetWidth);
+        console.log(this.parentNode.offsetHeight);
         this._setProperties();
     }
 
@@ -37,15 +40,31 @@ class Raindrop {
         this.properties.left = Math.ceil((gameWindow.offsetWidth - 50) * Math.random());
     }
 
-    addToScreen(parentNode) {
+    addToScreen() {
         this.raindropBody.classList.add('raindrop');
         this.raindropBody.style.top = this.properties.top + 'px';
         this.raindropBody.style.left = this.properties.left + 'px';
         this.raindropBody.innerHTML = 'iuius';
-        parentNode.appendChild(this.raindropBody);
+        this.parentNode.appendChild(this.raindropBody);
+    }
+
+    move(speed) {
+        console.log(this.raindropBody.style.top);
+        let coord = +this.raindropBody.style.top.slice(0, -2);
+        console.log(coord);
+        if (coord < this.parentNode.offsetHeight) {
+            coord += speed;
+        } else {
+            coord = 0;
+            this.properties.left = Math.ceil((this.parentNode.offsetWidth - 50) * Math.random());
+        };
+        this.raindropBody.style.top = +coord + 'px';
+        this.raindropBody.style.left = this.properties.left + 'px';
     }
 }
 
 let raindrop = new Raindrop();
-const gameWindow = document.querySelector('.game');
-raindrop.addToScreen(gameWindow);
+raindrop.addToScreen();
+setInterval(() => {
+    raindrop.move(10);
+}, 100);
