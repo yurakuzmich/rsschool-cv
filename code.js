2ab9b937a4925e7272e60a4ee3b6e0c217f6b0a4
Yury Kuzmich <yurakuzmich@gmail.com>
2020-11-22 23:18:57 +0300

    feat: add move func

--- code.js
+++ code.js
@@ -1,8 +1,9 @@
 
 
 const keyboard = document.getElementById('keyboard');
+const display = document.querySelector('.keyboard__display');
 const settingsPanel = document.querySelector('.settings-panel');
-const buttonNewGame = document.getElementById('newGameButton');
+const buttonNewGame = document.getElementById('new-game-button');
 const buttonSettings = document.getElementById('settings-button');
 const buttonSaveSettings = document.getElementById('save-settings-button');
 const buttonCloseSettings = document.querySelector('.settings-panel__close');
@@ -12,11 +13,14 @@ const buttonTest = document.getElementById('test-button');
 let game;
 let rainDrops = [];
 let raindropMove = false;
+display.value = 0;
 //Event Listeners
+window.addEventListener('keydown', keyBoardKeyPress);
 keyboard.addEventListener('click', (e) => {
     keyboardClick(e);
 });
 
+buttonNewGame.addEventListener('click', startNewGame);
 buttonSettings.addEventListener('click', toggleSettingsPanel);
 buttonSaveSettings.addEventListener('click', toggleSettingsPanel);
 buttonCloseSettings.addEventListener('click', toggleSettingsPanel);
@@ -24,11 +28,57 @@ buttonTest.addEventListener('click', () => { });
 
 //functions
 function keyboardClick(e) {
-    console.log(e.target.textContent);
+    let clicked = e.target;
+    if (clicked.classList.contains('keyboard__button-number')) {
+        if (display.value === '0') {
+            display.value = clicked.textContent;
+        } else {
+            display.value += clicked.textContent;
+        }
+    } else if (clicked.textContent === 'Clear') {
+        display.value = '0';
+    } else if (clicked.textContent === 'Del') {
+        let val = display.value.slice(0, -1);
+        val === '' ? display.value = '0' : display.value = val;
+    } else if (clicked.textContent === 'Enter') {
+        let answer = display.value;
+        rainDrops.forEach((raindrop, index) => {
+            if (raindrop.checkAnswer(answer)) {
+                rainDrops.splice(index, 1);
+            };
+        });
+    }
+}
+
+function keyBoardKeyPress(e) {
+    console.log(e);
+    if (e.keyCode >= 96 && e.keyCode <= 105) {
+        if (display.value === '0') {
+            display.value = e.key;
+        } else {
+            display.value += e.key;
+        }
+    } else if (e.keyCode === 8) {
+        let val = display.value.slice(0, -1);
+        val === '' ? display.value = '0' : display.value = val;
+    } else if (e.keyCode === 46) {
+        display.value = '0';
+    } else if (e.keyCode === 13) {
+        let answer = display.value;
+        rainDrops.forEach((raindrop, index) => {
+            if (raindrop.checkAnswer(answer)) {
+                rainDrops.splice(index, 1);
+            };
+        });
+    }
 }
 
 function startNewGame() {
     game = new Game();
+    setInterval(() => {
+        newRaindrop();
+    }, 4000);
+
 }
 
 function toggleSettingsPanel() {
@@ -38,7 +88,10 @@ function toggleSettingsPanel() {
 
 function newRaindrop() {
     let rainDrop = new Raindrop();
+    rainDrop.addToScreen();
+    setInterval(() => { rainDrop.move(10) }, 100);
     rainDrops.push(rainDrop);
+    console.log(rainDrops);
 }
 
 class Game {
@@ -99,7 +152,7 @@ class Raindrop {
         // console.log(this.raindropBody.style.top);
         let coord = +this.raindropBody.style.top.slice(0, -2);
         // console.log(coord);
-        if (coord < this.parentNode.offsetHeight) {
+        if (coord < this.parentNode.offsetHeight - 100) {
             coord += speed;
         } else {
             coord = 0;
@@ -141,10 +194,13 @@ class Raindrop {
             case ('-'):
                 this.task.firstOperand = Math.round(Math.random() * 100);
                 this.task.secondOperand = Math.round(Math.random() * 100);
-                if(this.task.firstOperand > this.task.secondOperand) {
+                if (this.task.firstOperand > this.task.secondOperand) {
                     this.task.answer = this.task.firstOperand - this.task.secondOperand;
                 } else {
-                    this.task.answer = this.task.secondOperand - this.task.firstOperand;
+                    let num = this.task.firstOperand;
+                    this.task.firstOperand = this.task.secondOperand;
+                    this.task.secondOperand = num;
+                    this.task.answer = this.task.firstOperand - this.task.secondOperand;
                 }
                 break;
             case ('*'):
@@ -155,10 +211,23 @@ class Raindrop {
             case ('/'):
                 this.task.firstOperand = Math.round(Math.random() * 10);
                 this.task.secondOperand = Math.round(Math.random() * 10);
+                if (this.task.secondOperand === 0) {
+                    this.task.secondOperand = 1;
+                }
                 let preRes = this.task.firstOperand * this.task.secondOperand;
                 this.task.firstOperand = preRes;
                 this.task.answer = preRes / this.task.secondOperand;
         }
+    }
+
+    checkAnswer(answer) {
+        if (+answer === +this.task.answer) {
+            console.log('CORRECT ANSWER');
+            display.value = '0';
+            this.raindropBody.remove();
+            return true;
+        }
+        return false;
     }
 
     delete() {
@@ -167,22 +236,4 @@ class Raindrop {
 }
 
 
-// let raindrop = new Raindrop();
-// raindrop.addToScreen();
 
-for(let i = 0; i < 3; i++) {
-    newRaindrop();
-}
-
-rainDrops.forEach(raindrop => {
-    raindrop.addToScreen();
-    setInterval(() => {
-        raindrop.move(Math.round(Math.random() * 10));
-    }, 100);
-});
-
-// raindropMove = setInterval(() => {
-//     raindrop.move(10);
-// }, 100);
-
-// setInterval(() => {new Raindrop();}, 1000);
